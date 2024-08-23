5fa68563374a187a2c50b5c06d74687efe101ed6
Smilari <smilari.lucas@gmail.com>
2024-08-23 14:14:00 -0300

    Creating NavBar (Both for Desktop and Mobile).

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const links = document.querySelector('.hamburger__links');
+    const icon  = document.querySelector('.hamburger__icon');
+
+    links.classList.toggle('open');
+    icon.classList.toggle('open');
+}
