f7417d9559b8bd0f37dc80ccbb74eebbc78fce34
Smilari <smilari.lucas@gmail.com>
2024-12-05 23:27:53 -0300

    Changing favicon

--- src/config/intersectionObserver.js
+++ src/config/intersectionObserver.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const sections = document.querySelectorAll('section');
-  console.log(sections);
   const navLinks = document.querySelectorAll('nav ul li a');
-  console.log(navLinks);
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
